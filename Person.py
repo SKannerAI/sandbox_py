ab0deec1331755aa0f9fe69e167f63651d80398f
SKannerAI <sandeep.kanparthy@gmail.com>
2025-09-15 17:09:19 -0400

    Delete Person.py

--- Person.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-
-p1 = Person('Sandeep', 43)
-
-print(f"Person name is {p1.name} and age is {p1.age}")
